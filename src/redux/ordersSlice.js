aae333f1d87517531dbc27c31413712db0aea44b
Anarasty <murloc.ninja16@gmail.com>
2025-07-15 09:41:32 +0300

    v4 code clean fix

--- src/redux/ordersSlice.js
+++ src/redux/ordersSlice.js
@@ -8,45 +8,6 @@ if (!initialState || initialState.length === 0) {
   saveState("orders", initialState);
 }
 
-// const ordersSlice = createSlice({
-//   name: 'orders',
-//   initialState,
-//   reducers: {
-//     addOrder: {
-//       reducer(state, action) {
-//         state.push(action.payload);
-//         saveState('orders', state);
-//       },
-//       prepare(order) {
-//         // return { payload: { ...order, id: nanoid() } };
-//         return {
-//           payload: {
-//             ...order,
-//             id: order.id || nanoid(), // <-- убедись, что есть эта проверка!
-//           },
-//         };
-//       },
-//     },
-//     updateOrder(state, action) {
-//       const index = state.findIndex(o => o.id === action.payload.id);
-//       if (index !== -1) {
-//         state[index] = action.payload;
-//         saveState('orders', state);
-//       }
-//     },
-//     deleteOrder(state, action) {
-//       const index = state.findIndex(o => o.id === action.payload);
-//       if (index !== -1) {
-//         state.splice(index, 1);
-//         saveState('orders', state);
-//       }
-//     },
-//   },
-// });
-
-// export const { addOrder, updateOrder, deleteOrder } = ordersSlice.actions;
-// export default ordersSlice.reducer;
-
 const ordersSlice = createSlice({
   name: "orders",
   initialState,
